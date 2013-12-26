378b1ac7510211c243643c7f3899bb168691e8f2
Peter Hagens <peterhagens@gmail.com>
2013-12-26 16:55:56 +0000

    index.html support?

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var buf = new Buffer();
+var stream = fs.readFileSync("index.html");
+var output = stream.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//  response.send('Hello World2!');
+  response.send("output"+output);
 });
 
 var port = process.env.PORT || 5000;
