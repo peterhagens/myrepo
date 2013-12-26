939a1aed018fb84ee3dcf461a9128561e47f34d7
Peter Hagens <peterhagens@gmail.com>
2013-12-26 19:04:53 +0000

    Edited index.html

--- web.js
+++ web.js
@@ -5,15 +5,13 @@ app.use(express.logger());
 var fs = require('fs');
 var buf = new Buffer('test');
 var stream = fs.readFileSync("index.html");
-var output = buf.toString();
 var output2 = stream.toString();
 
 app.get('/', function(request, response) {
-//  response.send('Hello World2!');
-  response.send("output"+output+"output2"+output2);
+  response.send(output2);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
